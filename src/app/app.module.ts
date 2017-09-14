d598c2e93f820e6e72d93a269af7a15902106876
gszumilas92 <gszumilas92@gmail.com>
2017-09-14 21:35:42 +0200

    General styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,17 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 
 import { Routes, PreloadAllModules, RouterModule } from "@angular/router";
+import { OfferComponent } from './offer/offer.component';
+import { ContactComponent } from './contact/contact.component';
+import { PrizesComponent } from './prizes/prizes.component';
 
 //Check Viability
 const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutMeComponent },
+    { path: 'offer', component: OfferComponent },
+    { path: 'prizes', component: PrizesComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '',
       redirectTo: '/home',
       pathMatch: 'full'
@@ -29,7 +35,10 @@ const appRoutes: Routes = [
     NavigationComponent,
     HomeComponent,
     FooterComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    OfferComponent,
+    ContactComponent,
+    PrizesComponent
   ],
   imports: [
     BrowserModule,
